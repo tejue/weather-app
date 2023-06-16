9936d6d2b3bd93e559a11b89f10fc6b845a3c01b
Teresa <teresa.juengel@posteo.de>
2023-06-16 17:01:48 +0200

    fix again

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import List from "./components/List/index.js";
 import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
-<<<<<<< HEAD
-  const [activities, setActivities] = useState([]);
-  function handleAddActivity(newActivity) {
-    setActivities([...activities, { id: uid(), ...newActivity }]);
-    console.log(newActivity);
-=======
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }]);
->>>>>>> fa46d8a (add list, functonality and local storage)
     console.log(activities);
   }
 

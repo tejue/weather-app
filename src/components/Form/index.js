20cf77c50879b2ce8db1e1e1a5f512a97d969261
Cornelius Mörig <c.moerig@gmail.com>
2023-06-16 15:26:18 +0200

    fix submit function so it works with checked

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,19 +3,19 @@ import "./Form.css";
 export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-    if (data.isForGoodWeather) {
-      data.isForGoodWeather = true;
-    } else {
-      data.isForGoodWeather = false;
-    }
+
+    const form = event.target;
+    const formElements = form.elements;
+
+    const data = {
+      name: formElements.name.value,
+      isForGoodWeather: formElements.isForGoodWeather.checked,
+    };
+    console.log(data);
     onAddActivity(data);
-
-    event.target.reset();
-    event.target.elements.name.focus();
+    form.reset();
+    formElements.name.focus();
   }
-
   return (
     <form className="form" onSubmit={handleSubmit}>
       <h2>Add new Activity:</h2>

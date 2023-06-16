f23705e80ca57bafb6bc971ccba09139daeaf6e4
Teresa <teresa.juengel@posteo.de>
2023-06-16 12:13:33 +0200

    add form same same but different

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import "./App.css";
-import Form from "./components/Form/index";
+import Form from "./components/Form/index.js";
 
 export default function App() {
   return (
     <>
-      <div className=".App">Test</div>;
       <Form />
     </>
   );
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,16 @@
-export default function Form() {
+import "./Form.css";
+
+export default function Form({ onAddActivity }) {
   return (
-    <>
+    <form className="form">
       <h2>Add new Activity:</h2>
-      <label htmlFor="nameInput">Name</label>
-      <input id="nameInput" type="checkbox">
-        Name:
-      </input>
-      <label htmlFor="weatherActivityCheckbox">Good-weather activity</label>
-      <input id="weatherActivityCheckbox"></input>
-      <button type="submit"></button>
-    </>
+      <label htmlFor="nameInput">Name:</label>
+      <input id="nameInput" type="text" />
+      <div>
+        <label htmlFor="weatherActivityCheckbox">Good-weather activity:</label>
+        <input id="weatherActivityCheckbox" type="checkbox" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }

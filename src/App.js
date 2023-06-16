923cf22dce74a90d4b13e83de6794926d1a4bdde
Teresa <teresa.juengel@posteo.de>
2023-06-16 16:38:58 +0200

    add list, functonality and local storage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import Form from "./components/Form/index.js";
-import { useState } from "react";
 import { uid } from "uid";
+import List from "./components/List/index.js";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+<<<<<<< HEAD
   const [activities, setActivities] = useState([]);
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }]);
     console.log(newActivity);
+=======
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { id: uid(), ...newActivity }]);
+>>>>>>> fa46d8a (add list, functonality and local storage)
     console.log(activities);
   }
 
   return (
-    <>
+    <div>
       <Form onAddActivity={handleAddActivity} />
-    </>
+      <List activities={activities} />
+    </div>
   );
 }

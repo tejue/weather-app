8ee8a39d2b77204e6ecea29862944be82ba26705
Teresa <teresa.juengel@posteo.de>
2023-06-16 17:48:29 +0200

    clean code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ export default function App() {
   });
   function handleAddActivity(newActivity) {
     setActivities([...activities, { id: uid(), ...newActivity }]);
-    console.log(activities);
   }
 
   return (
